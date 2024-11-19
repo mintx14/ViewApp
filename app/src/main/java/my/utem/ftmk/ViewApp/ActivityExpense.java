a6dba2ffb822723a73bf087ca1186d760dd10b49
ViewApp <haikalzabidi114@gmail.com>
2024-11-19 17:51:26 +0800

    commit

--- app/src/main/java/my/utem/ftmk/ViewApp/ActivityExpense.java
+++ app/src/main/java/my/utem/ftmk/ViewApp/ActivityExpense.java
@@ -6,16 +6,22 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Calendar;
 
@@ -25,12 +31,52 @@ public class ActivityExpense extends AppCompatActivity {
 
     ActivityExpenseBinding binding;
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityExpenseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        drawerLayout = binding.myDrawerLayout;
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open,
+                R.string.nav_close);
+
+        actionBarDrawerToggle.syncState();
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = binding.navigation;
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId())
+                {
+                    case R.id.nav_login_activity:
+                        intent = new Intent(getApplicationContext(), ActivityLogin.class);
+                        startActivity(intent);
+                        return true;
+
+                    case R.id.nav_register:
+                        intent = new Intent(getApplicationContext(), ActivityRegister.class);
+                        startActivity(intent);
+                        return true;
+
+                    case R.id.nav_expenses:
+                        intent = new Intent(getApplicationContext(), ActivityExpense.class);
+                        startActivity(intent);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
 
         Integer[] numbers = new Integer[15];
         for (int i = 0; i < 15; i++){
@@ -51,6 +97,7 @@ public class ActivityExpense extends AppCompatActivity {
                 fnInvokeDatePicker();
             }
         });
+
     }
 
     DatePickerDialog pickerDialog;
--- app/src/main/java/my/utem/ftmk/ViewApp/ActivityLogin.java
+++ app/src/main/java/my/utem/ftmk/ViewApp/ActivityLogin.java
@@ -3,19 +3,30 @@ package my.utem.ftmk.ViewApp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import my.utem.ftmk.ViewApp.databinding.ActivityLoginBinding;
 
 public class ActivityLogin extends AppCompatActivity {
 
      ActivityLoginBinding binding;
+
+
+     private DrawerLayout drawerLayout;
+     private ActionBarDrawerToggle actionBarDrawerToggle;
+     private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,50 @@ public class ActivityLogin extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+
+        drawerLayout = binding.myDrawerLayout;
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open,
+                R.string.nav_close);
+
+        actionBarDrawerToggle.syncState();
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = binding.navigation;
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId())
+                {
+                    case R.id.nav_login_activity:
+                        intent = new Intent(getApplicationContext(), ActivityLogin.class);
+                        startActivity(intent);
+                        return true;
+
+                    case R.id.nav_register:
+                        intent = new Intent(getApplicationContext(), ActivityRegister.class);
+                        startActivity(intent);
+                        return true;
+
+                    case R.id.nav_expenses:
+                        intent = new Intent(getApplicationContext(), ActivityExpense.class);
+                        startActivity(intent);
+                        return true;
+
+                    default:return false;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
