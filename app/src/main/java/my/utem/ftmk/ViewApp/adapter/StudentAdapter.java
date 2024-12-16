b760d6285d6519413c00ca1cf28f298a61b720ac
mintx14 <haikalzabidi114@gmail.com>
2024-12-17 01:04:41 +0800

    commit

--- app/src/main/java/my/utem/ftmk/ViewApp/adapter/StudentAdapter.java
+++ app/src/main/java/my/utem/ftmk/ViewApp/adapter/StudentAdapter.java
@@ -18,6 +18,7 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentV
     private final LayoutInflater layoutInflater;
     private final Vector<Student> students;
 
+    // Constructor for the adapter
     public StudentAdapter(LayoutInflater layoutInflater, Vector<Student> students) {
         this.layoutInflater = layoutInflater;
         this.students = students;
@@ -26,35 +27,43 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentV
     @NonNull
     @Override
     public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the item layout
         View view = layoutInflater.inflate(R.layout.item_student, parent, false);
         return new StudentViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
+        // Get the student object for the current position
         Student student = students.get(position);
-        holder.tvFullName.setText(student.getStrFullname());
-        holder.tvStudNo.setText(student.getStrStudNo());
-        holder.tvEmail.setText(student.getStrEmail());
-        holder.tvGender.setText(student.getStrGender());
-        holder.tvState.setText(student.getStrState());
+
+        // Bind data to the views
+        holder.lblFullName.setText(student.getStrFullname());
+        holder.lblStudNo.setText(student.getStrStudNo());
+        holder.lblEmail.setText(student.getStrEmail());
+        holder.lblGender.setText(student.getStrGender());
+        holder.lblState.setText(student.getStrState());
     }
 
     @Override
     public int getItemCount() {
+        // Return the total number of items
         return students.size();
     }
 
+    // ViewHolder class for Student
     static class StudentViewHolder extends RecyclerView.ViewHolder {
-        TextView tvFullName, tvStudNo, tvEmail, tvGender, tvState;
+        private final TextView lblFullName, lblStudNo, lblEmail, lblGender, lblState;
 
         public StudentViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvFullName = itemView.findViewById(R.id.tvFullName);
-            tvStudNo = itemView.findViewById(R.id.tvStudNo);
-            tvEmail = itemView.findViewById(R.id.tvEmail);
-            tvGender = itemView.findViewById(R.id.tvGender);
-            tvState = itemView.findViewById(R.id.tvState);
+
+            // Initialize the TextView fields
+            lblFullName = itemView.findViewById(R.id.lblFullName);
+            lblStudNo = itemView.findViewById(R.id.lblStudNo);
+            lblEmail = itemView.findViewById(R.id.lblEmail);
+            lblGender = itemView.findViewById(R.id.lblGender);
+            lblState = itemView.findViewById(R.id.lblState);
         }
     }
 }
